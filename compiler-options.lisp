6a5806c31f58d864c4fc1774aee7793ddde5fddd
Spencer Brody <sbrody@itasoftware.com>
2008-08-15 16:26:00 -0400

    Add README file for future developers. Add compiler-options.lisp to the git respository - somehow it must have been missed before and was never added.

---
+++ compiler-options.lisp
@@ -0,0 +1,27 @@
+(in-package :xcvb)
+
+(defvar *lisp-implementation* :sbcl)
+(defvar *lisp-executable-pathname* nil)
+;(defvar *lisp-executable-pathname* "/ita/trunk/sbcl/install/1.0.17.39.ita.3.cfasl/x86_64-linux/sbcl")
+;(defvar *lisp-executable-pathname* "/usr/local/openmcl/working-0711-10188/lx86cl64")
+(defvar *lisp-image-pathname* nil)
+(defvar *lisp-options* nil)
+(defvar *use-cfasls* t)
+
+
+#|(defvar *lisp-implementation* :sbcl)
+(defvar *lisp-executable-pathname* "/home/sbrody/xcvb/test/sbcl-with-setup.core")
+;(defvar *lisp-executable-pathname* "/ita/trunk/sbcl/install/1.0.17.39.ita.3.cfasl/x86_64-linux/sbcl")
+;(defvar *lisp-executable-pathname* "/usr/local/openmcl/working-0711-10188/lx86cl64")
+(defvar *lisp-image-pathname* nil)
+;(defvar *lisp-image-pathname* "/home/sbrody/xcvb/test/ccl-with-setup.core")
+(defvar *lisp-options* nil)
+(defvar *use-cfasls* t)|#
+
+#|
+(defvar *DEFAULT_LISP* "sbcl")
+(defvar *DEFAULT_CCL_VERSION* "working-0711-9670")
+(defvar *DEFAULT_SBCL_VERSION* "1.0.14.36.ita.1")
+(defvar *DEFAULT_CMUCL_VERSION* "cmucl-32on64-20060323")
+(defvar *DEFAULT_ALLEGRO_VERSION* "acl50")
+|#
