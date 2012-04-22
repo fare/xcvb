756181157a77a7b63735583d4338b184f2e97054
Francois-Rene Rideau <tunes@google.com>
2012-04-22 18:30:21 -0400

    Update dependencies in doc/Makefile.release. Tweak require-asdf.

--- require-asdf.lisp
+++ require-asdf.lisp
@@ -7,14 +7,15 @@
 
 ;; We assume that your implementation comes with ASDF.
 ;; If not, complain to your vendor.
-(require :asdf)
-(unless (or #+asdf2 (asdf:version-satisfies (asdf:asdf-version) "2.000"))
-  (error "XCVB requires ASDF2"))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (require :asdf)
+  (unless (or #+asdf2 (asdf:version-satisfies (asdf:asdf-version) "2.000"))
+    (error "XCVB requires ASDF2"))
 
-;; Make sure you have the latest ASDF *before* you load any other system.
-;; This matters when the implementation provides an ASDF earlier than 2.015.
-(handler-bind ((style-warning #'muffle-warning))
-  (asdf:load-system :asdf))
+  ;; Make sure you have the latest ASDF *before* you load any other system.
+  ;; This matters when the implementation provides an ASDF earlier than 2.015.
+  (handler-bind ((style-warning #'muffle-warning))
+    (asdf:load-system :asdf))
 
-(unless (asdf:version-satisfies (asdf:asdf-version) "2.019")
-  (error "XCVB requires ASDF 2.019 or later"))
+  (unless (asdf:version-satisfies (asdf:asdf-version) "2.019")
+    (error "XCVB requires ASDF 2.019 or later")))
